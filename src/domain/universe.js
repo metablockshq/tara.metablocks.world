97fbbc290cffd792951b73d95a3aa4a9a0a16dd3
Shivek Khurana <khuranashivek@outlook.com>
2022-02-18 21:14:47 +0530

    💂🏼 Demo ready (#2)

--- src/domain/universe.js
+++ src/domain/universe.js
@@ -2,10 +2,15 @@ import { PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
 import { defAtom } from "@thi.ng/atom";
 import useSWR from "swr";
 import { api } from "@kyraa/metablocks";
+import { Position, Toaster } from "@blueprintjs/core";
 
 import { programIds } from "../config";
 
 const state = defAtom({ depositedNfts: [] });
+
+export const UniverseToaster = Toaster.create({
+  position: Position.TOP,
+});
 
 // TODO: convert this to useResource hook
 const useUniverses = (network) => {
@@ -25,6 +30,7 @@ const taraUniverseKey = new PublicKey(
   "56AfWAaYWxKwLY4HbaanCjre6K8zvtLYy5v2465Wy3dd"
 );
 
+// WARNING: Don't use this op unless you want to send 500 requests to Solana
 const getUserNfts = async ({ wallet, connection, filter }) => {
   const res = await api.getWrappedUserNftAccounts(
     { wallet, connection },
@@ -39,24 +45,61 @@ const getUserNfts = async ({ wallet, connection, filter }) => {
   return res;
 };
 
+const shortenLongUrl = (longUrl) => {
+  const url = new URL("https://hideuri.com/api/v1/shorten");
+  let headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  };
+
+  let body = {
+    url: longUrl,
+  };
+
+  return fetch(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body),
+  }).then((response) => response.json());
+};
 const depositNft = async ({ wallet, connection, metadata }) => {
   const mintKey = new PublicKey(metadata?.data?.mint);
-  const url = metadata?.data?.data?.uri; // arweave URL
+  const arweaveUrl = metadata?.data?.data?.uri; // arweave URL
   const isReceiptMasterEdition = true; // users should not be able to create copies of receipt
-
+  const searchParams = new URLSearchParams({
+    arweaveUrl,
+    universeAddress: taraUniverseKey.toString(),
+    mintAddress: metadata?.data?.mint,
+    depositedAt: Date.now(),
+  });
+  /*
+   *   const url = `https://0-1-0--receipt-metadata-wrapper.metablockshq.autocode.gg/?${searchParams}`;
+   *
+   *
+   *   const urlShortenerRes = await shortenLongUrl(url);
+   *   const shortUrl = urlShortenerRes?.short_url;
+   *  */
   try {
     state.resetIn("depositingNft", true);
     const res = await api.depositNft({
       wallet,
       connection,
       mintKey,
-      url,
+      url: arweaveUrl,
       isReceiptMasterEdition,
       universeKey: taraUniverseKey,
     });
 
     state.resetIn("depositNftRes", res);
     console.log("---> deposit res", res);
+    UniverseToaster.show({
+      message: "NFT deposited successfully",
+      action: {
+        href: `https://explorer.solana.com/tx/${res.tx1}?cluster=devnet`,
+        text: "View",
+        target: "_blank",
+      },
+    });
   } catch (err) {
     console.log("depositNftError", err);
   } finally {
@@ -78,6 +121,14 @@ const withdrawNft = async ({ wallet, connection, metadata }) => {
 
     state.resetIn("withdrawNftRes", res);
     console.log("---> withdrawRes res", res);
+    UniverseToaster.show({
+      message: "NFT withdrawn successfully",
+      action: {
+        href: `https://explorer.solana.com/tx/${res}?cluster=devnet`,
+        text: "View",
+        target: "_blank",
+      },
+    });
   } catch (err) {
     console.log("withdrawNftError", err);
   } finally {
